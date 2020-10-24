3ed2027a0cd9f86a4fa518d0e155c38e97127881
gonzoibox <gonzoibox@gmail.com>
2020-10-24 20:35:28 +0300

    format code

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,18 +5,15 @@ import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
-
 export class HeroesComponent implements OnInit {
-
   heroes = HEROES;
   selectedHero: Hero;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
