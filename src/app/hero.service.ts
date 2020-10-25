08547b573db5bbaff47b429ac6f4f3ffd727d858
gonzoibox <gonzoibox@gmail.com>
2020-10-25 14:16:13 +0200

    add HttpClient , log(), URL  in hero.service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Hero } from './hero.model';
 import { HEROES } from './mock-heroes';
@@ -22,5 +23,15 @@ export class HeroService {
     return of(HEROES.find((hero) => hero.id === id));
   }
 
-  constructor(private messageService: MessageService) {}
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private heroesUrl = 'api/heroes'; // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 }
