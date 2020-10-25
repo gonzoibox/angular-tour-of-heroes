7b675382a6452b0a639518c9463b2d4d6704e0e7
gonzoibox <gonzoibox@gmail.com>
2020-10-25 14:56:21 +0200

    Update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,13 +21,21 @@ export class HeroService {
   }
 
   /** GET hero by id. Will 404 if id not found */
-getHero(id: number): Observable<Hero> {
-  const url = `${this.heroesUrl}/${id}`;
-  return this.http.get<Hero>(url).pipe(
-    tap(_ => this.log(`fetched hero id=${id}`)),
-    catchError(this.handleError<Hero>(`getHero id=${id}`))
-  );
-}
+  getHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((_) => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
 
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
@@ -54,6 +62,10 @@ getHero(id: number): Observable<Hero> {
       return of(result as T);
     };
   }
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
   constructor(
     private http: HttpClient,
